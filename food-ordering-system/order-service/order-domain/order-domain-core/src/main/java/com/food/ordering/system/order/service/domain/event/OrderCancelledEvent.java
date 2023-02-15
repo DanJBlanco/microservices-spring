08114404589c3322931b909fee1e8eedbcb25a9e
Daniel <djblancoh@gmail.com>
2023-02-15 00:08:58 -0500

    create events interfaces

---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent {
+    public OrderCancelledEvent(Order order, ZonedDateTime createAt) {
+        super(order, createAt);
+    }
+}
