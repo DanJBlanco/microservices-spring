b23c831c9f5964cc3b521fb5b57e96d56ded54a2
Daniel <djblancoh@gmail.com>
2023-02-18 20:53:11 -0500

    create controller, and exceptions handlers

--- food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/KafkaProducer.java
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/KafkaProducer.java
@@ -1,2 +1,12 @@
-package com.food.ordering.system.kafka.producer.service;public interface KafkaProducer {
+package com.food.ordering.system.kafka.producer.service;
+
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+public interface KafkaProducer <K extends Serializable, V extends SpecificRecordBase> {
+    void send(String topicName, K key, V message, CompletableFuture<SendResult<K, V>> callback);
 }
