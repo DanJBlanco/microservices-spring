87f60ccb15489970a7e009da9807dca1e73b9c1f
Daniel <djblancoh@gmail.com>
2023-02-22 18:08:54 -0500

    Application Service domain module: Adding mapper, Cinfig  and Ports

---
+++ food-ordering-system/payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/repository/CreditHistoryRepository.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.payment.service.domain.ports.output.repository;
+
+import com.food.ordering.system.payment.service.domain.entity.CreditHistory;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CreditHistoryRepository {
+    CreditHistory save(CreditHistory creditHistory);
+    Optional<List<CreditHistory>> findByCustomerId(UUID customerId);
+}
