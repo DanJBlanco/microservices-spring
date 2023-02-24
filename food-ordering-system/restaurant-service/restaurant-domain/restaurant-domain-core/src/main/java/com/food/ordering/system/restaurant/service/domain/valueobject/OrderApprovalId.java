c18f0789f8b075fb5f0b87f7d899f5a3e5c5247e
Daniel <djblancoh@gmail.com>
2023-02-23 23:04:41 -0500

    Create Restaurant Service Module, Domain Core module: Adding Aggregate Root and Entities

--- food-ordering-system/restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/valueobject/OrderApprovalId.java
+++ food-ordering-system/restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/valueobject/OrderApprovalId.java
@@ -1,2 +1,11 @@
-package com.food.ordering.system.restaurant.service.domain.valueobject;public class OrderApprovalId {
+package com.food.ordering.system.restaurant.service.domain.valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class OrderApprovalId extends BaseId<UUID> {
+    public OrderApprovalId(UUID value) {
+        super(value);
+    }
 }
