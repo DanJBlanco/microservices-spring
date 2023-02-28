02e0d0dcd65ebcf26976be87086b3c9fb12bb4b7
Daniel <djblancoh@gmail.com>
2023-02-27 23:59:57 -0500

    Updating Payment database schema, config and package structure for Outbox

--- food-ordering-system/payment-service/payment-container/src/main/resources/init-schema.sql
+++ food-ordering-system/payment-service/payment-container/src/main/resources/init-schema.sql
@@ -45,5 +45,31 @@ CREATE TABLE "payment".credit_history
     CONSTRAINT credit_history_pkey PRIMARY KEY (id)
 );
 
+DROP TYPE IF EXISTS outbox_status;
+CREATE TYPE outbox_status AS ENUM ('STARTED', 'COMPLETED', 'FAILED');
 
+DROP TABLE IF EXISTS "payment".order_outbox CASCADE;
+CREATE TABLE "payment".order_outbox
+(
+    id uuid not null ,
+    saga_id uuid not null ,
+    created_at timestamp with time zone not null ,
+    processed_at timestamp with time zone,
+    type character varying COLLATE pg_catalog."default" not null ,
+    payload jsonb not null ,
+    outbox_status outbox_status not null ,
+    payment_status payment_status not null ,
+    version integer not null ,
+    CONSTRAINT order_outbox_pkey PRIMARY KEY (id)
+);
+
+
+CREATE INDEX "payment_order_outbox_saga_status"
+    ON "payment".order_outbox
+        (type, payment_status);
+
+
+CREATE INDEX "payment_order_outbox_saga_id_payment_status_outbox_status"
+    ON "payment".order_outbox
+        (type, saga_id, payment_status, outbox_status);
 
