751bee401e45bfe857b1d7f2cc9c60b25be4cea4
Daniel <djblancoh@gmail.com>
2023-03-08 22:28:20 -0500

    Creating Customer Kafka topic & Customer modules

--- food-ordering-system/customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
+++ food-ordering-system/customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+
 public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
